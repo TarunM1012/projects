4c9597de402213463ec419f6827ce71fae455656
TarunM1012 <tarunm1012@gmail.com>
2020-03-28 09:35:52 -0400

    Initial commit

---
+++ learn/learn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using std::cout;
+
+int main() {
+    int a = 10;
+
+    std:: string b;
+    b = "Ten in my favourite number" ;
+    cout << a << "\n";
+    cout << b << "\n";
+}
